05c4f181a9c4ce2c0a4abaee08cc3f4f99f2b7c0
RijuSaha004 <rijusaha.burdwan@gmail.com>
2026-04-16 12:11:22 +0530

    add MongoDB connection configuration

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
 
 
 dotenv.config();
@@ -19,6 +20,13 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(port, () => {
-  console.log(`App is listing on port : ${port}`);
-});
+connectDB()
+  .then(() => {
+    console.log("DB connected");
+    app.listen(port, () => {
+      console.log(`App is listing on port : ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Server connection failed !!! : ", error);
+  });
